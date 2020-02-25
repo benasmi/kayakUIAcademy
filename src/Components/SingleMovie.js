1555eeef409dd39048e302896ff2f1f825486be4
Benas <benasmiliunas@gmail.com>
2020-02-25 19:45:08 +0200

    Displaying actual images

--- src/Components/SingleMovie.js
+++ src/Components/SingleMovie.js
@@ -8,8 +8,10 @@ export default class SingleMovie extends React.Component{
 
   render(){
 
-    console.log(this.props)
+    console.log(this.props.item)
 
+    const imgSrc = "https://image.tmdb.org/t/p/w500" + this.props.item.poster_path
+    console.log(imgSrc)
     const imgStyle = {
       width: "32px",
       height: "32px"
@@ -27,12 +29,12 @@ export default class SingleMovie extends React.Component{
     return(
       <div className="singleMovieContent" onClick={()=>this.props.handleCardClick(this.props.item)}>
         <div>
-          <img style={imgStyle} src="http://www.pngall.com/wp-content/uploads/2016/05/Trollface.png"/>
+          <img style={imgStyle} src={imgSrc}/>
         </div>
 
         <div style={infoDiv}>
           <div>{this.props.item.original_title}</div>
-          <div style={description}>{this.props.item.vote_average} Rating, {this.props.item.release_date}</div>
+          <div style={description}>{this.props.item.vote_average} Rating, {this.props.item.release_date.substring(0,4)}</div>
         </div>
       </div>
     )
